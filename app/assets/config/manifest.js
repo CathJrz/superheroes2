c019fa6a5f2b432000c3691e9f6bfb6228e001fe
Catherine Juarez <cathy.j18@gmail.com>
2017-03-28 03:59:49 -0600

    Commit 8 - Bootstrap

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,7 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+*= require bootstrap
+*= require bootstrap-theme
+*= require_tree .
+*= require_self
