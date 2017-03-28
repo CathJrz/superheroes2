bb7051709ac8c3c082fecd53779b8dc07d06ce7e
Catherine Juarez <cathy.j18@gmail.com>
2017-03-27 22:37:59 -0600

    Commit 5 - Intento de formularios

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,24 @@ class PagesController < ApplicationController
   end
 
   def batman_vs_superman
+    @name_batman = params[:name_batman]
+    @email_batman = params[:email_batman]
+    @votosbatman = Votosbatman.new(name:@name_batman, email:@email_superman)
+    @votosbatman.save
+
+    @name_superman = params[:name_superman]
+    @email_superman = params[:email_superman]
+    @votossuperman = Votossuperman.new(name:@name_superman, email:@email_superman)
+    @votossuperman.save
+
   end
 
+  def save_batmanfan
+  Votosbatman.create(name: params[:name_batman], email: params[:email_batman])
+  end
+
+  def save_supermanfan
+  Votossuperman.create(name: params[:name_superman], email: [:email_superman])
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'pages/batman'
   get 'pages/superman'
   get 'pages/batman_vs_superman'
+  post 'pages/save_batmanfan'
+  post 'pages/save_supermanfan'
   root 'pages#batman_vs_superman'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
